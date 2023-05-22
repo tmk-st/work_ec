452475aaa11e2304e33ed470aae8c3016295984a
Tomoka Sato <tomokasato@MacBookPro.local>
2023-05-22 22:16:56 +0900

    0522

--- js/sample.js
+++ js/sample.js
@@ -46,3 +46,29 @@ sliders.forEach((slider, cont_slider) => {
     });
   });
 });
+
+// カウントダウンタイマー
+const day = document.getElementById("day");
+const hour = document.getElementById("hour");
+const min = document.getElementById("min");
+const sec = document.getElementById("sec");
+
+function countdown() {
+  const now = new Date();
+  const after = new Date(now.getFullYear(),now.getMonth(),now.getDate()+3); // 3日後の0:00を取得
+  const diff = after.getTime() - now.getTime(); // 時間の差を取得（ミリ秒）
+
+  // ミリ秒から単位を修正
+  const calcDay = Math.floor(diff / 1000 / 60 / 60 / 24);
+  const calcHour = Math.floor((diff / 1000 / 60 / 60) % 60) - 48;
+  const calcMin = Math.floor(diff / 1000 / 60) % 60;
+  const calcSec = Math.floor(diff / 1000) % 60;
+
+  // 取得した時間を表示（2桁表示）
+  day.innerHTML = calcDay < 10 ? '0' + calcDay : calcDay;;
+  hour.innerHTML = calcHour < 10 ? '0' + calcHour : calcHour;
+  min.innerHTML = calcMin < 10 ? '0' + calcMin : calcMin;
+  sec.innerHTML = calcSec < 10 ? '0' + calcSec : calcSec;
+}
+countdown();
+setInterval(countdown,1000);
